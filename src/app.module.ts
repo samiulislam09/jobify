32cb7d2a1babe471cfc01fcc98f8ed0b077abbed
samiulislam09 <samiulislamsawon09@gmail.com>
2023-11-21 11:33:29 +0600

    added authentication pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/users.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
